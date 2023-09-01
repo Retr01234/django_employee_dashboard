307ca65e7f606d0437def7ba27c021f0dbb677e8
Pave <pitcheroty@gmail.com>
2023-09-01 15:28:27 +0200

    fix: change project name for heroku compatibility

--- employee-dashboard/employee-dashboard/urls.py
+++ employee-dashboard/employee-dashboard/urls.py
@@ -1,4 +1,4 @@
-"""employee_dashboard URL Configuration
+"""employee-dashboard URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
