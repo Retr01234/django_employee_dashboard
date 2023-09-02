0a083921d2b3b47573e6dffd208c96b208089328
Pave <pitcheroty@gmail.com>
2023-09-02 19:19:24 +0200

    feature: add form logic

--- project/app/urls.py
+++ project/app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from app import views
 
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('add', views.addStaff, name="addStaff")
 ]
--- project/app/views.py
+++ project/app/views.py
@@ -4,3 +4,13 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "index.html")
+
+def addStaff(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        gender = request.POST.get('gender')
+        email = request.POST.get('email')
+        number = request.POST.get('number')
+        title = request.POST.get('title')
+        
+    return render(request, "index.html")
