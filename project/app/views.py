fe4476beeba64e403774faade65503c62622cc77
Pave <pitcheroty@gmail.com>
2023-09-02 19:33:44 +0200

    feature: connect Views with DB Models

--- project/app/views.py
+++ project/app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .models import Employee
 
 # Create your views here.
 def index(request):
@@ -12,5 +12,15 @@ def addStaff(request):
         email = request.POST.get('email')
         number = request.POST.get('number')
         title = request.POST.get('title')
+        
+        dbQuery = Employee(
+            name = name,
+            gender = gender,
+            email = email,
+            number = number,
+            title = title
+        )
+        
+        dbQuery.save()
         
     return render(request, "index.html")
