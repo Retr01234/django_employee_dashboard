999f3a409c512dd20ff93337bf903038e8c5e361
Pave <pitcheroty@gmail.com>
2023-09-03 12:50:26 +0200

    fix: edit functionality bug

--- project/app/urls.py
+++ project/app/urls.py
@@ -3,5 +3,7 @@ from app import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('add', views.addStaff, name="addStaff")
+    path('add', views.addStaff, name="addStaff"),
+    path('delete/<id>', views.deleteStaff, name="deleteStaff"),
+    path('edit/<id>', views.editStaff, name="editStaff"),
 ]
--- project/app/views.py
+++ project/app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Employee
 
 # Create your views here.
@@ -27,3 +27,32 @@ def addStaff(request):
         dbQuery.save()
         
     return render(request, "index.html")
+
+def deleteStaff(request, id):
+    employee_list = Employee.objects.all()
+    employee_details = {"employee_list": employee_list}
+    
+    return render(request, "index.html", employee_details)
+
+def editStaff(request, id):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        gender = request.POST.get('gender')
+        email = request.POST.get('email')
+        number = request.POST.get('number')
+        title = request.POST.get('title')
+    
+        dbQuery = Employee(
+            name = name,
+            gender = gender,
+            email = email,
+            number = number,
+            title = title
+        )
+    
+        dbQuery.save()
+
+    employee_list = Employee.objects.get(id=id)
+    employee_details = {"employee_list": employee_list}
+    
+    return render(request, "edit.html", employee_details)
