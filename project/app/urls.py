ab44c040b3583d2c3a39bf4c88e197b11fb2eeec
Pave <pitcheroty@gmail.com>
2023-09-04 21:23:04 +0200

    feature: create login feature

--- project/app/urls.py
+++ project/app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.views.generic.base import TemplateView
 from app import views
 
 urlpatterns = [
-    path('', views.index, name="index"),
+    path('', TemplateView.as_view(template_name='base.html'), name='base'),
+    path('index', views.index, name="index"),
     path('add', views.addStaff, name="addStaff"),
     path('delete/<id>', views.deleteStaff, name="deleteStaff"),
     path('edit/<id>', views.editStaff, name="editStaff"),
